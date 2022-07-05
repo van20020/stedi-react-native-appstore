166fb9d1b4bdcdbd1007c3e0d2f78730f1165c58
sarii96 <sarah-96325@hotmail.com>
2022-07-05 08:48:11 -0600

    help page form

--- screens/Help.js
+++ screens/Help.js
@@ -1,124 +1,54 @@
-// import React from 'react';
-// import { StyleSheet, Text, View, Image, Linking } from 'react-native';
-// import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
-// import Bar  from './Bar.js';
-// import { Separator } from 'native-base';
-
-
-
-// const _header =(item) =>{
-//     return(
-//         <Separator bordered style={{alignItems:'center'}}>
-//           <Text>{item.title}</Text>
-//         </Separator>
-//     );
-// }
-
-// const _body =(item) =>{
-//     return (
-//         <View style={{padding:10}}>
-//           <Text style={{textAlign:'center'}}>{item.body}</Text>
-//         </View>
-//     );
-// }
-
-// const Help = () =>{
-//   const [list, setList] = useState([]);
-//   setList(
-//     [
-//       {
-//         id:1,
-//         title: 'Getting Started',
-//         body: 'React native Accordion/Collapse component, very good to use in toggles & show/hide content'
-//       },
-//       {
-//         id:2,
-//         title: 'Components',
-//         body: 'AccordionList,Collapse,CollapseHeader & CollapseBody'
-//       }
-//       ],
-
-//   )
-//   return(
-//     <View>
-//       <Bar/>
-// <Collapse>
-//     <CollapseHeader>
-//       <View>
-//         <Text>Click here</Text>
-//       </View>
-//     </CollapseHeader>
-//     <CollapseBody>
-//       <Text>Ta daa!</Text>
-//     </CollapseBody>
-// </Collapse>
-// <AccordionList
-//         list={state.list}
-//         header={_header}
-//         body={_body}
-//         keyExtractor={item => item.key}
-//       />
-//   </View>
-
-//   );
-// }
-
-// export default Help;
-
-{/* const styles = StyleSheet.create({
-  text:{
-   marginLeft:15,
-   marginRight:15,
-   fontSize: 18,
-    height: 44,
-  }
-}) */}
-
-
-
-import React from 'react';
-import { View,Text, StyleSheet } from 'react-native';
-import {Collapse, CollapseHeader, CollapseBody} from "accordion-collapse-react-native";
-
+import React, { useState} from 'react';
+import { View,Text, StyleSheet, TextInput } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 
 const Help = (props) =>{
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [subject, setSubject] = useState(null);
+  const [message, setMessage] = useState(null);
+  this.state ={
+    Subject: ' '
+  }
+
 return(
 <View>
-    <Collapse >
-      <CollapseHeader >
-        <View>
-          <Text style={styles.text}>Contact us</Text>
-        </View>
-      </CollapseHeader>
-      <CollapseBody>
-        <Collapse>
-          <CollapseHeader>
-          <Text style={styles.text2}>Phone: 333 333 333</Text>
-          <Text style={styles.text2}>Email: testing@hotmail.com</Text>
-          </CollapseHeader>
-          <CollapseBody>
-          </CollapseBody>
-        </Collapse>
-      </CollapseBody>
-    </Collapse>
-    <Collapse >
-      <CollapseHeader >
-        <View>
-          <Text style={styles.text}>Contact us</Text>
-        </View>
-      </CollapseHeader>
-      <CollapseBody>
-        <Collapse>
-          <CollapseHeader>
-          <Text style={styles.text2}>Phone: 333 333 333</Text>
-          <Text style={styles.text2}>Email: testing@hotmail.com</Text>
-          </CollapseHeader>
-          <CollapseBody >
-          </CollapseBody>
-        </Collapse>
-      </CollapseBody>
-    </Collapse>
+<Text style={styles.text}>Contact us</Text>
+<TextInput 
+ style={styles.input}
+ placeholder="Name"
+ onChangeText={(val)=> setName(val)}
+ />
+
+<TextInput
+        style={styles.input}
+        // onChangeText={newEmail=> setName(newEmail)}
+        placeholder="Email"
+      />
+  <DropDownPicker style={styles.dropdown}
+          items={[
+              {label: 'The STEDI app is not working', value: 'The STEDI '},
+              {label: 'I have an idea for improving STEDI', value: 'I have'},
+              {label: 'I need help using the app', value: 'I need'},
+          ]}
+          defaultIndex={0}
+          containerStyle={{height: 40}}
+          onChangeItem={item => console.log(item.label, item.value)}
+      />
+ <TextInput
+        style={styles.input2}
+        multiline
+        numberOfLines={8}
+        onChangeText={(val)=> setMessage(val)}
+        placeholder="Message"
+      />
+{/* <TouchableOpacity
+title='Submit'
+onPress={()}
+style={styles.button} >
+</TouchableOpacity> */}
   </View>
 )}
 export default Help;
@@ -131,9 +61,37 @@ export default Help;
   //  fontWeight: 'bold',
     height: 44,
   },
-  text2:{
-    marginLeft:15,
-   marginRight:15,
-   fontSize: 17,
+  input: {
+    // height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding:5,
+    borderRadius: 5,
+  },
+  input2:{
+    margin: 12,
+    borderWidth: 1,
+    padding:5,
+    borderRadius: 5,
+    marginTop:25
+  },
+  button: {
+  // marginTop: 15,
+  // marginBottom: 20,
+  // width: 200,
+  // height: 35,
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: 10,
+  borderRadius: 5,
+  // backgroundColor: 'orange',
+  // marginLeft:50
+  },
+  dropdown:{
+  width: 336,
+  height:40,
+  marginLeft:12,
+  marginBottom:10
+
   }
-}) 
+})
