a0f9d0c9d9a0a5b1659ac7a6a451d641594d9bf9
Sean Murdock <scmurdock@gmail.com>
2022-10-21 09:19:00 -0600

    Added textinput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage,TextInput, Button } from 'react-native';
 import  Navigation from './components/Navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
@@ -13,6 +13,8 @@ const AppStack = createNativeStackNavigator();
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [isLoggedIn,setIsLoggedIn] = React.useState(false);
+  const [phoneNumber,setPhoneNumber] = React.useState("");
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
 
    if (isFirstLaunch == true){
@@ -20,9 +22,60 @@ return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
 );
-  }else{
+  }else if(isLoggedIn){
     return <Navigation/>
+  } else{
+    return (
+      <View>
+        <TextInput 
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          style={styles.input}  
+          placeholderTextColor='#4251f5' 
+          placeholder='Cell Phone'>          
+        </TextInput>
+        <Button
+          title='Send'
+          style={styles.button}
+          onPress={async ()=>{
+            console.log(phoneNumber+' Button was pressed')
+
+            await fetch(
+              'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+              {
+                method:'POST',
+                headers:{
+                 'content-type':'application/text'
+               }
+              }
+            )
+          }}
+        />        
+      </View>
+    )
   }
 }
  export default App;
 
+ 
+ const styles = StyleSheet.create({
+     container:{
+         flex:1, 
+         alignItems:'center',
+         justifyContent: 'center'
+     },
+     input: {
+       height: 40,
+       marginTop: 12,
+       borderWidth: 1,
+       padding: 10,
+     },
+     margin:{
+       marginTop:100
+     },
+     button: {
+       alignItems: "center",
+       backgroundColor: "#DDDDDD",
+       padding: 10
+     }    
+ })
