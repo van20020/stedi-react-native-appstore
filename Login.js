053ecb435f6b5caf09e887ca286aa4cded1ff1cf
Ricky Vang <vangcheemeng@yahoo.com>
2023-05-16 10:13:18 -0600

    added login.js file with code

---
+++ Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+export default function Login(props){
+    return(
+        <View style={styles.login}>
+            <Text>This is the Login Screen</Text>
+            <Button title="Log In" onPress={()=>props.setUserLoggedIn(true)}></Button>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    login: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: 'green',
+        height: '12%',
+        alignItems: 'flex-end',
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        
+      },
+})
